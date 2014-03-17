59e7890340ca709124a51d94684ccf3e08d0733e
AidanPetit <aidan.petit@mail.mcgill.ca>
2014-03-17 13:48:48 -0400

    Initial commit with files written by Wei-Di

---
+++ FinalProject/src/PoseProvider.java
@@ -0,0 +1,13 @@
+import lejos.util.Timer;
+import lejos.util.TimerListener;
+import lejos.robotics.localization.OdometryPoseProvider;
+import lejos.robotics.navigation.MoveProvider;
+
+public class PoseProvider extends OdometryPoseProvider {
+
+	public PoseProvider(MoveProvider mp) {
+		super(mp);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
