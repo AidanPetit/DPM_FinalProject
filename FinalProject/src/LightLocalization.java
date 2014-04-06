7519df07eac6ce82758e5ae03f49ef4c8caa6a16
AidanPetit <aidan.petit@mail.mcgill.ca>
2014-04-06 14:37:11 -0400

    Added run-time CS calibration to determine average light value. This will be used, along with a threshold, to detect lines for LightLocalization.

--- FinalProject/src/LightLocalization.java
+++ FinalProject/src/LightLocalization.java
@@ -12,26 +12,31 @@ import lejos.robotics.navigation.Pose;
 
 /**
  *
- * Light Localization class which takes care of initial localization
- *
+ * Light Localization class which takes care of initial precise localization
+ * Corner Numbers:
+ * 1 - bottom left
+ * 2 - bottom right
+ * 3 - top right
+ * 4 - top left
  *
  * @author Aidan Petit
- * @version 1.0
+ * @version 2.0
  * @since 1.0
  */
 
 public class LightLocalization {
-	/*
-	 * This will probable be fairly buggy, I need to be in the lab to debug
-	 */
-
 	private OdometryPoseProvider odo;
-	private ColorSensor myLS;
+	private ColorSensor myCS;
 	private Navigator myNav;
 	private Driver myPilot;
 
 	private double rotateSpeed = 60;
 	private double sensorOffset = 12; // in cm, not accurate needs to be measured
+
+	public int LS_THRESHOLD = 50; //this value needs to be experimented with!
+
+	private double averageLight;
+
 
 	public LightLocalization(Team08Robot myBot) {
 		/*
@@ -40,20 +45,23 @@ public class LightLocalization {
 		 */
 
 		this.odo = myBot.getOdo();
-		this.myLS = myBot.getRightCS();
+		this.myCS = myBot.getRightCS();
 		this.myNav = myBot.getNav();
 		this.myPilot = myBot.getPilot();
 	}
 
-	public void doLocalization() {
+	public void doLocalization(int startingCorner) {
 		/*
-		 * This localization assumes the robot is at roughly a 0 degrees heading
-		 * and is located inside the bottom-left box.
+		 * This localization assumes the robot's heading is approximately
+		 * correct and it's center of rotation is inside one of
+		 * the starting corner boxes.
 		 *
 		 * Localization is performed by rotating in place to detect 4 grid lines
 		 * then performing some basic trigonometry to calculate the robots X and Y position
 		 * and a more accurate value for the heading
 		 */
+		
+		averageLight = calibrateLS();
 
 		boolean anglesClocked = false;
 		int lockCount = 0;
@@ -63,47 +71,46 @@ public class LightLocalization {
 
 		double xAxisIntersectAngle = 0; 	//holds angle when lightsensor hits negative X axis
 		double yAxisIntersectAngle = 0;
-
+		
 
 		// start rotating and clock all 4 gridlines
 
 		myNav.rotateTo(0);
-		myLS.setFloodlight(true);
+		myCS.setFloodlight(true);
 
 		myPilot.setRotateSpeed(rotateSpeed);
 		myPilot.rotateRight();
 
 		while(!anglesClocked){ //keep rotating until all four angles have been clocked
 
-			int currentReading = myLS.getNormalizedLightValue(); //get a new value for the light reading every iteration
+			
+			int currentReading = myCS.getNormalizedLightValue(); //get a new value for the light reading every iteration
 
-			//			LCD.drawString("LS: "+ currentReading, 0, 6); //output for debugging, might need to disable LCDDisplay to use
+			LCD.clear(5);
+			LCD.drawString("LS: "+ currentReading, 0, 5); //output for debugging, might need to disable LCDDisplay to use
 			//			LCD.drawString("count: "+ lockCount, 0, 7); //output for debugging, might need to disable LCDDisplay to use
 
-			/*
-			 * this uses a threshold. this is a bad method we should use a
-			 * differential method to detect the change in light instead.
-			 * Juan and I never implemented this
-			 */
 
-			if (currentReading<470) {
+//			if (currentReading<470) {	//old, implementing calibrated average threshold method for etecting lines
+				
+			if(currentReading < averageLight - LS_THRESHOLD) {
 				double currentTheta = odo.getPose().getHeading();
 				if (lockCount==0){ // negative x axis
 					xAngles[0] = currentTheta;
 					xAxisIntersectAngle = currentTheta; //save the negative x axis intersection angle for future reference
 					lockCount++;
 				}
-				else if (lockCount==1){ //negative y
+				else if (lockCount==1){ //positive y
 					yAngles[0] = currentTheta;
-					yAxisIntersectAngle = currentTheta;
 					lockCount++;
 				}
 				else if (lockCount==2){ //positive x
 					xAngles[1] = currentTheta;
 					lockCount++;
 				}
-				else if (lockCount==3){ //positive y
+				else if (lockCount==3){ //negative y
 					yAngles[1] = currentTheta;
+					yAxisIntersectAngle = currentTheta;
 					lockCount++;
 				}
 
@@ -145,46 +152,80 @@ public class LightLocalization {
 		double thetaX = Math.abs(xAngles[1]-xAngles[0]);
 
 
-		double X = -sensorOffset*Math.cos((thetaY/2)*Math.PI/180);
-		double Y = -sensorOffset*Math.cos((thetaX/2)*Math.PI/180);
+		double dX = sensorOffset*Math.cos((thetaY/2)*Math.PI/180);
+		double dY = sensorOffset*Math.cos((thetaX/2)*Math.PI/180);
 		//calculate your new positions using thetaX and thetaY
 
 		double newHeadingY = 90 + (thetaX/2) - (xAxisIntersectAngle-180);
-		double newHeadingX = 90 + (thetaY/2) - (yAxisIntersectAngle-180);
+		//double newHeadingX = 90 + (thetaY/2) - (yAxisIntersectAngle-180);
 
-		//		newHeadingY = fixAngle2(newHeadingY);
-
-		//		double newHeading = fixAngle2((newHeadingX+newHeadingY)/2);
-
-		//		LCD.drawString("tX: " + thetaX, 0, 3);
-		//		LCD.drawString("tY: " + thetaY, 0, 4);
-		//		
-		//		
-		//		LCD.drawString("newX: "+ newHeadingX, 0, 6); //output for debugging, might need to disable LCDDisplay to use
-		//		LCD.drawString("newY: "+ newHeadingY, 0, 7); //output for debugging, might need to disable LCDDisplay to use
-		//
 
 		/*
 		 *  initialize a new Pose to update the Odometer
 		 *  X and Y are cast to float, precision shouldnt be an issue
 		 */
 		double currentTheta = fixAngle(odo.getPose().getHeading());
-		double newTheta = fixAngle2(currentTheta+(newHeadingY-45));
+		double newTheta = fixAngle2(currentTheta+(newHeadingY-52.5));
 
-		float newX = (float) X;
-		float newY = (float) Y;
-		float newT = (float) newTheta;
+		//set the odometer to reflect the new X, Y and theta values		
+		if(startingCorner == 1) {
+			float newX = (float) (0.0-dX);
+			float newY = (float) (0.0-dY);
+			float newT = (float) newTheta;
+			Pose newPos = new Pose(newX,newY,newT);
+			odo.setPose(newPos);
+			myNav.goTo(0,0);
 
-		Pose pos = new Pose(newX,newY,newT);
 
-		//set the odometer to reflect the new X, Y and theta values
-		odo.setPose(pos);
+		}
+		else if(startingCorner == 2) {
+			float newX = (float) (304.8+dX);
+			float newY = (float) (0.0-dY);
+			float newT = (float) newTheta;
+			Pose newPos = new Pose(newX,newY,newT);
+			odo.setPose(newPos);
+			myNav.goTo((float)304.8,(float)0.0);
 
-		myNav.goTo(0,0);
+		}
+		else if(startingCorner == 3) {
+			float newX = (float) (304.8-dX);
+			float newY = (float) (304.8-dY);
+			float newT = (float) newTheta;
+			Pose newPos = new Pose(newX,newY,newT);
+			odo.setPose(newPos);
+			myNav.goTo((float)304.8,(float)304.8);
 
-		try { Thread.sleep(2000); } catch (InterruptedException e) {}  //this delay is to make sure a line is not detected twice
-		
-		myNav.rotateTo(0);
+		}
+		else if(startingCorner == 4) {
+			float newX = (float) (0.0-dX);
+			float newY = (float) (304.8-dY);
+			float newT = (float) newTheta;
+			Pose newPos = new Pose(newX,newY,newT);
+			odo.setPose(newPos);
+			myNav.goTo((float)0.0,(float)304.8);
+
+		}
+
+		while(myNav.isMoving()) {
+			//do nothing
+		}
+
+		if(startingCorner == 1) {
+			myNav.rotateTo(0);
+		}
+		else if(startingCorner == 2){
+			myNav.rotateTo(90);
+		}
+		else if(startingCorner == 3){
+			myNav.rotateTo(180);
+
+		}
+		else if(startingCorner == 4){
+			myNav.rotateTo(-90);
+		}
+
+		try { Thread.sleep(15000); } catch (InterruptedException e) {}  //this delay is to make sure a line is not detected twice
+
 
 	}
 
@@ -205,6 +246,22 @@ public class LightLocalization {
 		if(angle>180) angle-=360;
 		return angle;
 
+	}
+	
+	public double calibrateLS() {
+			
+		double result = 0;
+
+		for (int i = 0; i < 10; i++) {
+			result += myCS.getNormalizedLightValue();
+		}
+
+		result = result/10;
+		
+		LCD.clear(6);
+		LCD.drawString("AVG:" + result, 0,6);
+
+		return result;
 	}
 
 }
